3058ef4e9e03a789eee21237e037c3899ef3adac
Linsmar <linsmarvital@gmail.com>
2021-05-10 23:19:37 +0100

    Criado comando help

--- main.js
+++ main.js
@@ -7,26 +7,50 @@ const dbadjetivos = require("./db/dbadjetivos")
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-//Função dos Adjetivos
+
+var prefix = '$';
+const comandos = [prefix + 'adj', prefix + 'nanahj', prefix + 'prefix', prefix + 'avatar']
+
 client.on('message', msg => {
-  if (msg.content.includes('$adj')) {
+  if (msg.author.bot) return;
+  //Função dos Adjetivos
+  if (msg.content.includes(prefix + 'adj')) {
     let item = dbadjetivos.adjetivos.flat()[Math.floor(Math.random() * dbadjetivos.adjetivos.flat().length)];
     let x = msg.content.replace("$adj", item);
     msg.channel.send("**" + msg.author.username + "** disse: " + x);
   }
-});
-//Função da nana hoje
-client.on('message', msg => {
-  if (msg.content.startsWith('$nanahj')) {
+  //Função da nana hoje
+  if (msg.content.startsWith(prefix + 'nanahj')) {
     var item = dbnanaroles.nanaroles[Math.floor(Math.random() * dbnanaroles.nanaroles.length)];
     msg.channel.send("Hoje a Nana está... " + item);
+  }
+  //Função trocar prefix
+  if (msg.content.startsWith(prefix + 'prefix')) {
+    var split = msg.content.split(' ');
+    if (split.length > 2 || split[0].length > 7 || !split[1]) return;
+    let novoPrefix = split[1].length == 1? split[1] : null;
+    if (novoPrefix) {
+      prefix = novoPrefix;
+      return msg.channel.send("O novo prefix é **" + prefix + "**");
+    }
+  }
+  //Função help
+  if (msg.content === prefix + 'help') {
+    var listaComandos = 'Os comandos são: \n';
+    comandos.map((e, i) => {
+      listaComandos += i+1 + '. ' + e + '\n'
+    })
+    return msg.channel.send(
+      listaComandos
+    )
   }
 });
 
 
+
 //Função de avatar
 client.on('message', async msg => {
-  if (!msg.content.startsWith('$avatar') || msg.content.split(' ').length > 2) return
+  if (!msg.content.startsWith(prefix + 'avatar') || msg.content.split(' ').length > 2 || msg.author.bot) return
   const mensagens = msg.content.split(' ')
   const ping = msg.mentions.users.first()
 
