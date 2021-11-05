19c8426c98e405387eaecfa8347505c82562e3da
Linsmar <linsmarvital@gmail.com>
2021-11-06 00:17:46 +0100

    Alive again

--- db/dbnanaroles.js
+++
@@ -1,4 +0,0 @@
-exports.nanaroles = ["Palhaçinha da Kraken", "Nataria", "Satânananista", "Nanazista", "Mercenanaria", "Fofonana", "Tadinhana", "Cagonana", "Nanarcisista", 
-"Nana lindo", "A Nata da Kraken", "Anã Nana", "Sacanati", "Banana", "Jájá", "Prenanacoce", "Ladinana", "IllumiNati", "Nanaviu", "Trynanahard", "Donanati", 
-"Nana ni na não", "Swagnatti", "Nanão é uma boa pessoa", "Trananasfobica", "Nanafante", "Choconana", "Ladronana", "Mussa Nana", "Dronana", "Perdedora"
-]
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
-const dbnanaroles = require("./db/dbnanaroles")
 const dbadjetivos = require("./db/dbadjetivos")
 
 client.on('ready', () => {
@@ -18,11 +17,6 @@ client.on('message', msg => {
     let item = dbadjetivos.adjetivos.flat()[Math.floor(Math.random() * dbadjetivos.adjetivos.flat().length)];
     let x = msg.content.replace("$adj", item);
     msg.channel.send("**" + msg.author.username + "** disse: " + x);
-  }
-  //Função da nana hoje
-  if (msg.content.startsWith(prefix + 'nanahj')) {
-    var item = dbnanaroles.nanaroles[Math.floor(Math.random() * dbnanaroles.nanaroles.length)];
-    msg.channel.send("Hoje a Nana está... " + item);
   }
   //Função trocar prefix
   if (msg.content.startsWith(prefix + 'prefix')) {
